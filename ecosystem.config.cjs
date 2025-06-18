6684cc22a40595d0972a02dba9de0e3b16dfa076
eao2 <enkh.amar.001@gmail.com>
2025-06-19 04:50:43 +0800

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,7 +8,9 @@ module.exports = { apps: [
             NITRO_PORT: 4100,
             DIVISION_LOCATION_ID: 'cm9vnxb4w0000s5d4debydiiq',
             NODE_ENV: 'production',
-            DATABASE_URL: "mysql://admin:neto24@localhost:3306/neto"
+            DATABASE_URL: "mysql://admin:neto24@localhost:3306/neto",
+            JWT_SECRET: 'BC879C9583AD29F34FACC7970B36C1E58D8ABBC785025B3A197BB0A4655669C6'
+
         }
     }
     ]
