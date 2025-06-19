44d6792b6e247e4adc9e676e129d5070c2b0c7c5
eao2 <enkh.amar.001@gmail.com>
2025-06-20 05:04:38 +0800

    a

--- server/api/division/division-name.js
+++ server/api/division/division-name.js
@@ -2,7 +2,7 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
   const divisionId = event.context.auth.divisionId
 
   if (!divisionId) {
